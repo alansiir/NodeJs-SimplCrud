23336fe3e67ef2a2d769e7e07376be3df6c68612
alansiir <ala.nsir.pr@gmail.com>
2024-10-06 22:05:17 +0100

    connexion Base

--- app.js
+++ app.js
@@ -3,21 +3,26 @@ const express = require("express");
 const app = express();
 const port = 3000;
 
-// const mongoose = require("mongoose");
+const mongoose = require("mongoose");
 
 app.get("/", (req, res) => {
     res.sendFile(__dirname + "/views/Home.html");
 });
 
 
-app.listen(port, () => {
-  console.log(`http://localhost:${port}/`);
-});
 
 
-// mongoose.connect("Liiiiiiiiiiiiiiiiiiiiiiiiiiiiiiink")
-// .then(() => {})
-// .catch((err) => {console.log(err)});
+
+mongoose
+.connect("mongodb+srv://alansir101:zH7HEQuohYAAASDa@cluster0.shce2.mongodb.net/all-data?retryWrites=true&w=majority&appName=Cluster0")
+.then(() => {
+
+  app.listen(port, () => {
+    console.log(`http://localhost:${port}/`);
+  });
+
+})
+.catch((err) => {console.log(err)});
 
 
 
