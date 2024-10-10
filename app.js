15b9ad0d19efb11e740fe7cb43f72c9580e6135d
alansiir <ala.nsir.pr@gmail.com>
2024-10-11 00:18:57 +0100

    Read Data

--- app.js
+++ app.js
@@ -4,6 +4,8 @@ const app = express();
 const port = 3000;
 const mongoose = require("mongoose");
 app.use(express.urlencoded({ extended :true }));
+
+const Costomer  = require("./models/customerscuma")
 const Mydata =require("./models/myscuma")
 app.set('view engine','ejs')
 app.use(express.static('public'))
@@ -17,6 +19,8 @@ const liveReloadServer = livereload.createServer();
 // Spécifie le répertoire à surveiller (par exemple, "public")
 liveReloadServer.watch(path.join(__dirname, 'public'));
 const connectLivereload = require('connect-livereload');
+const { render } = require("ejs");
+const { redirect } = require("express/lib/response");
 // Ajouter le middleware connect-livereload
 app.use(connectLivereload());
 liveReloadServer.server.once("connection",() => {
@@ -25,10 +29,35 @@ liveReloadServer.server.once("connection",() => {
   },100)
 })
 //*********************************** */
+ // method Post
+ app.post("/user/add.html", (req, res) => {
+     const costomer = new Costomer(req.body)
+     costomer.save().then(() => {
+      res.redirect("/")
+     }).catch(() => {
+      
+     })
+    
+   
+    
+  });
+  
+  
 
 
+
+
+
+ // method Get
 app.get("/", (req, res) => {
-res.render("index",{ });
+  Costomer.find().then((resulet) => {
+    
+    res.render("index",{ arr :resulet});
+  }).catch((err) => {
+    console.log(err)
+  })
+
+
 }
 );
 
@@ -51,6 +80,8 @@ app.get("/user/edit.html", (req, res) => {
   res.render("user/edit");
     }
     );
+
+
 
 
 
---
+++ models/customerscuma.js
@@ -0,0 +1,21 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+//definie schema
+const customerSchema = new Schema({
+  FirstName : String,
+  LastName : String,
+  Email : String,
+  Telephone : String,
+  Age : String,
+  Country : String,
+  Gender : String
+
+
+});
+
+//creaction modele
+const Customer = mongoose.model("customer", customerSchema);
+
+//export modele
+module.exports = Customer;
